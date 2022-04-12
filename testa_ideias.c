e687e190417627b51de2c507d1120a8fd877a755
Dante-ELeuterio <danteeleuterio00@gmail.com>
2022-04-12 16:43:40 -0300

    comentarios

--- testa_ideias.c
+++ testa_ideias.c
@@ -37,16 +37,30 @@ int main(){
     for (int i = 0; i < 100; i++)
         heap[i]=0;    
     iniciaAlocador();
-    alocaMem(13);
-    heap[14]=289;
+
+    long int *x=alocaMem(100/8+1);
+    heap[3]=789;
     *topoheap=358;
+
+    //testa o tamanho do vetor
     printf("tamanho=%ld\n",sizeof(heap));
+    //testa se setou certo bit de ocupado e tamanho alocado
     printf("(1)heap[0]=%ld\n",*topoinicialheap);
     printf("(100)heap[1]=%ld\n",*(topoinicialheap+1));
+    //testa se os endereços de heap[0] e o ponteiro pro inicio batem
     printf("(e)heap[0]=%p\n",heap);
     printf("(e)topoiniciaheap=%p\n",topoinicialheap);
+    //testa se os endereços de heap[2] e x batem
+    printf("(e)heap[2]=%p\n",&heap[2]);
+    printf("(e)x=%p\n",x);
+    //testa se os endereços de heap[14] e o topo da heap batem
     printf("(e)heap[14]=%p\n",&heap[14]);
     printf("(e)topoheap=%p\n",topoheap);
+
+    //testa se os endereços são os mesmos e se alterar o valor de um altera o outro
+    heap[3]=789;
+    *topoheap=358;
+    printf("x=%ld\n",*(x+1));
     printf("heap[14]=%ld\n",heap[14]);
     printf("topoheap=%ld\n",*topoheap);
     
