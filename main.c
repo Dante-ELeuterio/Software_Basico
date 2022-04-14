0142d0aebf16621abdc7ffe55f8e67097af16885
Dante-ELeuterio <danteeleuterio00@gmail.com>
2022-04-14 14:43:59 -0300

    alocação 99%

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-extern batata();
+extern long int* alocaMem();
+extern void iniciaAlocador();
 int main(int argc, char const *argv[])
 {
-    batata();
+    iniciaAlocador();
+    long int *x=alocaMem(1000);
+    printf("%d\n",x);
+    printf("%d\n",*(x+1));
+    printf("%d\n",*(x+2));
+    
+    long int *y=alocaMem(500);
+    printf("%d\n",y);
+    printf("%d\n",*(y+1));
+    printf("%d\n",*(y+2));
     return 0;
 }
--- programa.s
+++ programa.s
@@ -2,13 +2,16 @@
     TOPO_HEAP: .quad 0                  #Ponteiro para o topo da Heap, nesse momento ela está vazia
     INICIO_HEAP: .quad 0                #Ponteiro fixo para o início da heap
     ANDARILHO: .quad 0                  #Ponteiro que caminha pela heap em alocaMem
+    BYTES_A_ALOCAR: .quad 0
+    TESTE: .quad 0
 
     str1: .string "TOPO_HEAP %d\n"
     str2: .string "INICIO_HEAP %d\n"
     str3: .string "PONTEIRO DA MEMORIA ALOCADA %d\n"
-
+    strteste: .string "TESTE:%d\n"
 .section .text
-.globl batata
+.globl alocaMem 
+.globl iniciaAlocador
 
 iniciaAlocador:
     pushq %rbp
@@ -25,6 +28,7 @@ iniciaAlocador:
 alocaMem:
     pushq %rbp
     movq %rsp,%rbp
+    movq %rdi, BYTES_A_ALOCAR
     movq INICIO_HEAP, %rax              #Ponteiro que caminha pela heap recebe o começo da Heap
     movq %rax, ANDARILHO
     movq TOPO_HEAP, %rbx
@@ -40,7 +44,7 @@ alocaMem:
     movq 16(%rax), %rbx                 #%rbx recebe o tamanho do espaco alocado no nodo
     addq %rbx, %rax
     addq $16, %rax                      #%rax desloca o tamanho do espaco alocado + 16 bytes
-    addq %rax, ANDARILHO                #ANDARILHO se desloca para o proximo nodo
+    movq %rax, ANDARILHO                #ANDARILHO se desloca para o proximo nodo
     movq ANDARILHO, %rax
     addq $8, %rax
     movq TOPO_HEAP, %rbx
@@ -51,15 +55,15 @@ alocaMem:
     ConfereTamanho:
     movq ANDARILHO, %rax
     movq 16(%rax), %rbx
-    movq 16(%rbp), %rax
+    movq BYTES_A_ALOCAR, %rax
     cmpq %rbx, %rax
     jg Else
     movq ANDARILHO, %rax                #pendencia de implementar no caso de nao ter espaco para as flags
-    movq 16(%rbp), %rcx
+    movq BYTES_A_ALOCAR, %rcx
     movq %rcx, 16(%rax)
-    movq 16(%rbp), %rax
-    subq %rax, %rbx
-    addq $16, %rbx
+    movq %rcx, %rax
+    subq %rax, %rbx                     
+    addq $16, %rbx                      #se alocar o mesmo tamanho ou n tiver espaço sobrando da merda
     movq ANDARILHO, %rax
     addq %rbx, %rax
     movq $0, 8(%rax)
@@ -72,43 +76,17 @@ alocaMem:
     ret                                 #Retorna para a main com o valor do endereco alocado em %rax
 
     AlocaEspacoNovo:
-    movq 16(%rbp), %rbx
+    movq BYTES_A_ALOCAR,%rbx
     addq $16, %rbx
     addq TOPO_HEAP, %rbx
     movq $12, %rax
     movq %rbx, %rdi
     syscall
-    movq %rbx, TOPO_HEAP
+    movq %rax, TOPO_HEAP
     movq ANDARILHO, %rax
-    movq $1, 8(%rax)                    #Seta o sinal de livre no novo espaço alocado
-    movq 16(%rbp), %rbx                 #Armazena o tamanho do bloco no campo de tamanho em ANDARILHO + 16 bytes
+    movq $1, 8(%rax)                    #Seta o sinal de ocupado no novo espaço alocado
+    movq BYTES_A_ALOCAR, %rbx           #Armazena o tamanho do bloco no campo de tamanho em ANDARILHO + 16 bytes
     movq %rbx, 16(%rax)
-    movq INICIO_HEAP, %rbx
-    movq %rbx, ANDARILHO
     pop %rbp
     ret                                 #Retorna para a main com o valor do endereco alocado em %rax
-
-batata:
-    pushq %rbp
-    movq %rsp,%rbp
-    call iniciaAlocador
-    mov $str1,%rdi
-    mov TOPO_HEAP,%rsi
-    call printf
-    mov $str2,%rdi
-    mov INICIO_HEAP,%rsi
-    call printf
-
-    pushq $1000                         #Teste de alocaMem(1000)
-    call alocaMem
-    addq $8,%rsp
-    mov $str1,%rdi
-    mov TOPO_HEAP,%rsi
-    call printf
-    mov $str3, %rdi
-    mov %rax, %rsi
-    call printf
-
-    pop %rbp
-    ret
 
